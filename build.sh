11ee5379b3de140811a5416d2bf50f6bb200162a
zouyq <zyqcn@live.com>
2021-09-06 12:05:00 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,9 +8,5 @@ IMAGE_NAME="$PARENT_DIR/$CURRENT_DIR"
 #TAG="${1}"
 
 REGISTRY="ghcr.io"
-
 docker build -t ${REGISTRY}/${IMAGE_NAME}:latest .
-
-echo ${{ secrets.PACKAGES_TOKEN }} | docker login --username ${{ secrets.DOCKER_USERNAME }} --password-stdin ${REGISTRY}
-
 docker push ${REGISTRY}/${IMAGE_NAME}
