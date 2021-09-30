46cfeba6bd82fb0f05e8df832e9ec62260aaace7
zouyq <zyqcn@live.com>
2021-09-30 16:16:41 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ CURRENT_DIR="${PWD##*/}"
 IMAGE_NAME="$PARENT_DIR/$CURRENT_DIR"
 #TAG="${1}"
 
-REGISTRY="ghcr.io"
+#REGISTRY="ghcr.io"
+REGISTRY=""
 docker build -t ${REGISTRY}/${IMAGE_NAME}:latest .
 docker push ${REGISTRY}/${IMAGE_NAME}
