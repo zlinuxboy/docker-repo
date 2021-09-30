e9c6191151d941936173c5da2fe20ff998b848eb
zouyq <zyqcn@live.com>
2021-09-30 16:57:28 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ set -ex
 PARENT_DIR=$(basename "${PWD%/*}")
 CURRENT_DIR="${PWD##*/}"
 #IMAGE_NAME="$PARENT_DIR/$CURRENT_DIR"
-IMAGE_NAME="zouyq/$CURRENT_DIR"
+IMAGE_NAME="$CURRENT_DIR"
 #TAG="${1}"
 
 #REGISTRY="ghcr.io"
 REGISTRY=""
-docker build -t ${IMAGE_NAME}:latest .
-docker push ${REGISTRY}/${IMAGE_NAME}
+docker build -t zouyq/${IMAGE_NAME}:latest .
+docker push zouyq/${IMAGE_NAME}
